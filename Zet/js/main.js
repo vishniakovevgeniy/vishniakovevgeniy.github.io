c5b9883cbd816200e3c43e1c5883589355ee959b
evgeny <veshniakovevgeniy@gmail.com>
2017-10-27 00:37:00 +0300

    second init

---
+++ Zet/js/main.js
@@ -0,0 +1,33 @@
+/** Выпадающее меню */
+$(document).ready(function() {
+
+  $(".out_btn").click(function() {
+    $(".top_nav").slideToggle("slow", function() {
+    });
+  });
+});
+$(document).ready(function() {
+  $(".top_nav").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+        top = $(id).offset().top;
+    $("body,html").animate({scrollTop: top}, 1000);
+  });
+});
+
+$(document).ready(function() {
+    $(".info").on("click", "a", function(event) {
+      event.preventDefault();
+      var id = $(this).attr("href"),
+          top = $(id).offset().top;
+      $("body,html").animate({scrollTop: top}, 1500);
+    });
+  });
+
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    mode: 'fade',
+    autoDirection: 'next',
+    auto: true
+  });
+});
